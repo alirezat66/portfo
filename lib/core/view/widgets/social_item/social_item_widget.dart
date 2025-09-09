5db94d724cb05bdf97bef9b572ef7e29e3a9a5fb
Reza Taghizadeh <alirezataghizadeh@gmail.com>
2025-09-09 12:02:41 +0200

    Enhance SocialIconWidget with hover effects and dynamic styling. Implement MouseRegion and StatefulBuilder to improve user interaction by changing border width and icon size on hover. Refactor existing code for better maintainability and responsiveness.

--- lib/core/view/widgets/social_item/social_item_widget.dart
+++ lib/core/view/widgets/social_item/social_item_widget.dart
@@ -7,27 +7,38 @@ import 'package:url_launcher/url_launcher.dart';
 class SocialIconWidget {
   static Widget card(BuildContext context, Social social,
       {double iconSize = 32}) {
-    return GestureDetector(
-      onTap: () => launchUrl(Uri.parse(social.url)),
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          color: context.basicColors.backgroundColor,
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(
-            color: context.basicColors.surfaceBrandColor.withValues(alpha: 0.3),
-            width: 1,
+    bool isHovered = false;
+
+    return StatefulBuilder(
+      builder: (context, setState) {
+        return MouseRegion(
+          onEnter: (_) => setState(() => isHovered = true),
+          onExit: (_) => setState(() => isHovered = false),
+          child: GestureDetector(
+            onTap: () => launchUrl(Uri.parse(social.url)),
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: context.basicColors.backgroundColor,
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(
+                  color: context.basicColors.surfaceBrandColor
+                      .withValues(alpha: 0.3),
+                  width: isHovered ? 3 : 1,
+                ),
+              ),
+              child: SvgPicture.asset(
+                social.icon,
+                width: iconSize,
+                height: iconSize,
+                fit: BoxFit.none,
+                colorFilter: ColorFilter.mode(
+                    context.basicColors.textSecondaryColor, BlendMode.srcIn),
+              ),
+            ),
           ),
-        ),
-        child: SvgPicture.asset(
-          social.icon,
-          width: iconSize,
-          height: iconSize,
-          fit: BoxFit.none,
-          colorFilter: ColorFilter.mode(
-              context.basicColors.textSecondaryColor, BlendMode.srcIn),
-        ),
-      ),
+        );
+      },
     );
   }
 
@@ -37,23 +48,30 @@ class SocialIconWidget {
     double iconSize = 20,
     double radius = 20,
   }) {
-    return Tooltip(
-      message: social.tooltip,
-      child: GestureDetector(
-        onTap: () => launchUrl(Uri.parse(social.url)),
-        child: CircleAvatar(
-          radius: iconSize,
-          backgroundColor: context.colorScheme.tertiary,
-          child: SvgPicture.asset(
-            social.icon,
-            colorFilter: ColorFilter.mode(
-                context.colorScheme.onTertiary, BlendMode.srcIn),
-            width: iconSize,
-            height: iconSize,
+    bool isHovered = false;
+    return StatefulBuilder(builder: (context, setState) {
+      return MouseRegion(
+        onEnter: (_) => setState(() => isHovered = true),
+        onExit: (_) => setState(() => isHovered = false),
+        child: Tooltip(
+          message: social.tooltip,
+          child: GestureDetector(
+            onTap: () => launchUrl(Uri.parse(social.url)),
+            child: CircleAvatar(
+              radius: iconSize,
+              backgroundColor: context.basicColors.surfaceTertiaryColor,
+              child: SvgPicture.asset(
+                social.icon,
+                colorFilter: ColorFilter.mode(
+                    context.colorScheme.onTertiary, BlendMode.srcIn),
+                width: iconSize * (isHovered ? 1.1 : 1),
+                height: iconSize * (isHovered ? 1.1 : 1),
+              ),
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 
   // Easy to add new styles without modifying existing code
